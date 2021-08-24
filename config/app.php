825f376c81843e294d025ce1a07ca33601211f00
leo <lrmaldo@gmail.com>
2021-08-24 16:41:35 -0500

    cambios en el composer

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
        
         /* pdf */
-        Barryvdh\DomPDF\ServiceProvider::class,
+       /*  Barryvdh\DomPDF\ServiceProvider::class, */
 
         /*
          * Package Service Providers...
@@ -230,7 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+        
      
 
     ],
